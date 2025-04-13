ad82e78d6e623f9ee2070986e5af9b2a00327fef
DominikVIA <144497193+DominikVIA@users.noreply.github.com>
2025-04-13 22:50:19 +0200

    initial commit

---
+++ src/routes/About.jsx
@@ -0,0 +1,28 @@
+import "../index.css"
+
+export default function About() {
+    return (
+        <>
+            <div id="about">
+                <div>
+                    <img src="src\assets\sinnoh_logo.png"/>
+                    <h2>About!</h2>
+                    <img src="src\assets\sinnoh_logo.png"/>
+                </div>
+                <p>
+                    This is our (Group 7!) submition for the second course assignment of the second web course. <br/>
+                    There were a few requirements for this submission:
+                    <ul>
+                        <li>the source of the information should be PokeApi</li>
+                        <li>the pokedex should display a list of pokemon and include pagination</li>
+                        <li>there should be more information displayed when clicked on a pokemon entry</li>
+                        <li>the application should contain multiple pages and use React Router to link them together</li>
+                        <li>the application should be developed with Vite</li>
+                    </ul>
+                    We decided to style it after the BDSP Sinnoh pokedex, because it seemed like the easiest to replicate (:<br/>
+                    Unfortunately the "Pokemon details" page proved a bit too difficult so we took it a bit easy.
+                </p>
+            </div>
+        </>
+    )
+}
